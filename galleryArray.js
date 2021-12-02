590a044ee73901da5c4b514779c210460c0f36db
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-02 14:44:44 -0800

    adds flex grid to gallery

--- galleryArray.js
+++ galleryArray.js
@@ -5,16 +5,44 @@ galleryArray = [
     'https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 ]
 
+const galleryContainer = document.getElementById('gallery');
 const uploadBtn = document.getElementById('upload-btn');
 let uploadImg = '';
 
-uploadBtn.addEventListener('change', function() {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-        uploadImg = reader.result
-        document.getElementById('gallery').style.backgroundImage = `url(${uploadImg})`
-    })
-    reader.readAsDataURL(this.files[0])
-})
+// uploadBtn.addEventListener('change', function() {
+//     const reader = new FileReader();
+//     reader.addEventListener('load', () => {
+//         uploadImg = reader.result
+//         console.log(uploadImg)
+//         galleryArray.push(uploadImg)
+        
+//         // document.getElementById('gallery').style.backgroundImage = `url(${uploadImg})`
+//     })
+//     reader.readAsDataURL(this.files[0])
+// })
+
+// function createImg() {
+//     return
+//             `<div class="img-card">
+//                 <img src='' alt="Gallery Image"/>
+//             </div>
+//             `
+// }
+
+function renderImg() {
+    let htmlString = '';
+    for (let i = galleryArray.length - 1; i >= 0; i--) {
+        const newImg =   `<div class="img-card">
+                            <img class="card" src=${galleryArray[i]} alt="Gallery Image"/>
+                          </div>
+                         `
+        htmlString += newImg;
+    }
+    galleryContainer.innerHTML = htmlString
+}
+renderImg()
 
 console.log('hi')
+//within listener, create html for each photo that is for each item
+//loop through to put them on the dom
+//make the gallery.innerHTML = the string of the looped items
