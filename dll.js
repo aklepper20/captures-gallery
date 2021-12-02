09671151601d33007273bc423e8012f40efcf099
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-02 11:57:22 -0800

    adds javascript imports

---
+++ dll.js
@@ -0,0 +1,30 @@
+export class Node {
+    constructor(value, next) {
+      this.value = value;
+      this.next = next || null;
+      this.prev = null;
+    }
+  }
+  
+  export default class DoubleLinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+    
+    push(value) {
+      const node = new Node(value);
+
+      if (this.length == 0) {
+        this.head = node;
+        this.tail = node;
+      } else {
+
+        this.tail.next = node;
+        this.tail = node;
+        node.prev = this.tail;
+      }
+      this.length++;
+    }
+}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hi')
+import DoubleLinkedList from './dll';
+import images from './images';
+import './style.css';
+
+const viewer = document.getElementById('viewer');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
